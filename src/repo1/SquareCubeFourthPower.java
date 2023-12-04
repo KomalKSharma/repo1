413c4309f3b13601bd627aeda9a78d731dea4ea7
Komal Sharma <komal.sharma11095@gmail.com>
2023-12-04 19:12:59 +0530

    commit

---
+++ src/repo1/SquareCubeFourthPower.java
@@ -0,0 +1,14 @@
+package repo1;
+
+public class SquareCubeFourthPower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 2;
+		System.out.println("Square is : " + (x*x));
+		System.out.println("Cube is : " + (x*x*x));
+		System.out.println("Fourth Power is : " + (x*x*x*x));
+
+	}
+
+}
