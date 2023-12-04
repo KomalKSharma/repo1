f778113d017a72e1fcd4776a1550900cca5f4860
Komal Sharma <komal.sharma11095@gmail.com>
2023-12-04 19:00:58 +0530

    commit

---
+++ src/repo1/ArithmeticOperators.java
@@ -0,0 +1,27 @@
+package repo1;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 6;
+		
+		System.out.println("Sum: " + (a + b));
+		System.out.println("Difference: " + (a - b));
+		System.out.println("Product: " + (a * b));
+		System.out.println("Average: " + ((a + b)/2));
+		System.out.println("Distance: " + (a - b));
+		if (a > b) {
+		System.out.println("Max: " + a);
+		}
+		
+		
+		if (b < a) {
+			System.out.println("Min: " + b);
+		}
+		
+
+	}
+
+}
