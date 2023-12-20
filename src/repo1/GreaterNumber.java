f173507b265ec3b5ca38af41bfe87b5d8e86ea55
Komal Sharma <komal.sharma11095@gmail.com>
2023-12-20 16:38:31 +0530

    commit

--- src/repo1/GreaterNumber.java
+++ src/repo1/GreaterNumber.java
@@ -12,16 +12,15 @@ public class GreaterNumber {
 			int a = scan.nextInt();
 			int b = scan.nextInt();
 			int c = scan.nextInt();
-			
 			if(a>b) {
-			System.out.println("a is greater than b i.e., " + a);
-			} else if(b>c) {
-				System.out.println("b is greater than c i.e.," + b);
-			} else {
+			System.out.println("a is greater i.e., " + a); 
+			} if(b>c) {
+				System.out.println("b is greater i.e.," + b);
+ 			} else {
 				System.out.println("c is greater i.e.," + c);
+			}
 			}
 		}
 
-	}
 
 }
