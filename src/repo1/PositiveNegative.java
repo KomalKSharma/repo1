80a43b73632d0710f845cd7ed9c3049f51a47f8a
Komal Sharma <komal.sharma11095@gmail.com>
2023-12-20 17:12:05 +0530

    positive and negative commits

---
+++ src/repo1/PositiveNegative.java
@@ -0,0 +1,24 @@
+package repo1;
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+		System.out.println("Enter number to test it positive or negative: ");
+		int input = scan.nextInt();
+		if(input>=0) {
+			System.out.println("Number is positive");
+			
+		} else {
+			System.out.println("Number is negative");
+		}
+		}
+		
+
+	}
+
+}
