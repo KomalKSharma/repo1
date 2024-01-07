31b816cd75770880a57f5287e538a9121a4545f4
Komal Sharma <komal.sharma11095@gmail.com>
2024-01-07 15:00:28 +0530

    commit

---
+++ src/repo1/Palindrome.java
@@ -0,0 +1,30 @@
+package repo1;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter input to check palindrome : " );
+		int k1 = scan.nextInt();
+		
+		int k2 = Integer.reverse(k1);
+		
+		System.out.println("reverse" + k2);
+		
+		if(k1 == k2) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/repo1/StringCharacters.java
@@ -0,0 +1,15 @@
+package repo1;
+
+public class StringCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String k1 = "this is java";
+		
+		System.out.println("string characters are: " + k1.replace(" ", "").length());
+		
+
+	}
+
+}
