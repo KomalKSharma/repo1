6941edd88b653c32a2a17d1a6fe582e46f80d907
Komal Sharma <komal.sharma11095@gmail.com>
2024-01-30 23:05:15 +0530

    commit

--- src/repo1/IntializeVarByConstructor.java
+++ src/repo1/IntializeVarByConstructor.java
@@ -5,11 +5,16 @@ public class IntializeVarByConstructor {
 
 	public static void main(String[] args) {
 		IntializeVarByConstructor k1 = new IntializeVarByConstructor(10);
+		k1.display();
 	}
 	
 	IntializeVarByConstructor(int i){
 		this.i = i;
 		System.out.println("value of i is: " + i);
 	}
+	
+	public void display() {
+	      System.out.println("Display i is: " + i);
+	   }
 
 }
