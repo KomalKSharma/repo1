d0819d78eed0bc466fde593374dc3c0d89c7167c
Komal Sharma <komal.sharma11095@gmail.com>
2024-03-11 22:36:27 +0530

    commit

--- src/repo1/Palindrome.java
+++ src/repo1/Palindrome.java
@@ -8,8 +8,27 @@ public class Palindrome {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		System.out.println("enter input to check palindrome : " );
-		int k1 = scan.nextInt();
+		String input = scan.next();
 		
+			char[] arr = input.toCharArray();
+			//char[] charArr = new char[input];
+			
+			//String inputValues = input;
+			String reverseString = "";
+			for(int i= arr.length-1; i>=0; i-- ) {
+				reverseString = reverseString+arr[i];
+			}
+			System.out.println("value of : " + reverseString);
+		
+		boolean Result = input.equals(reverseString);  
+		if(Result) {
+			System.out.println("this is the palindrome: true");
+		}else {
+			System.out.println("this is not palindrome: false");
+		}
+		
+		
+		/*
 		int k2 = Integer.reverseBytes(k1);
 		
 		System.out.println("reverse" + k2);
@@ -19,6 +38,8 @@ public class Palindrome {
 		} else {
 			System.out.println("False");
 		}
+		
+		*/
 		
 		
 		
